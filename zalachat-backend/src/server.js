4ffb6967e1001824c3e52c58be04af69688c1e56
HoGiaKham <hogiakham1705@gmail.com>
2025-06-20 10:22:24 +0700

    update code

--- zalachat-backend/src/server.js
+++ zalachat-backend/src/server.js
@@ -18,7 +18,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "https://zala-frontend.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -31,7 +31,7 @@ const cognitoISP = new AWS.CognitoIdentityServiceProvider({
 });
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL || "https://zala-frontend.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
@@ -523,7 +523,6 @@ io.on("connection", (socket) => {
 });
 
 // Start the Server
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
